35c6e49933c72579d2d5c7d181b1b569479b5a74
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-06 03:52:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def homepage():
 	if(DEBUG):
 		print('Response Status - ', response.status)
 		
-	return jsonify(attendancecontent)
+	return (attendancecontent)
 		
 	response, logoutcontent = http.request(URL + '/logout', 'GET', headers=headers, body=body)
 	if(DEBUG):
