771312daa656848a3ea3545486f86ba85469b6ef
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-06 01:30:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def homepage():
 	if(DEBUG):
 		print('Response Status - ', response.status)
 		
-	return (logincontent + attendancecontent)
+	return flask.jsonify(logincontent + attendancecontent)
 		
 	response, logoutcontent = http.request(URL + '/logout', 'GET', headers=headers, body=body)
 	if(DEBUG):
