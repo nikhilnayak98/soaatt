f96112a303255b0fc53afa7f1d8470c797862694
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-09 02:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 http = httplib2.Http()
 
 # endpoint parameters
-URL = 'http://111.93.164.203/CampusPortalSOA'
+URL = 'http://111.93.164.202:8282/CampusPortalSOA'
 reglov = 'ITERRETD1711A0000002'
 membertype = 's'
 headers = 0
