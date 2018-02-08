f567b69b07d99256deb90e7f3793a73831bdaf95
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-09 00:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ app = Flask(__name__)
 
 http = httplib2.Http()
 
+headers = ''
+body = ''
+
 @app.route('/')
 def my_form():
+	return render_template('login.html', error=0)
+
+@app.route('/logout')
+def logout():
+	response, logoutcontent = http.request(URL + '/logout', 'GET', headers=headers, body=body)
 	return render_template('login.html', error=0)
 
 @app.route('/', methods=['POST'])
@@ -18,6 +26,10 @@ def homepage():
 	# endpoint
 	URL = 'http://111.93.164.203/CampusPortalSOA'
 	reglov = 'ITERRETD1711A0000002'
+	
+	# global var
+	global body
+	global headers
 	
 	# html content
 	htmlcontent = '<html> <head> <title>Attendance Info</title><meta name="viewport" content="width=device-width, initial-scale=1"><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> <link type="text/css" rel="stylesheet" href="static/material/css/materialize.css" media="screen,projection"/> <meta name="viewport" content="width=device-width, initial-scale=1.0"/> </head> <body>'
@@ -58,19 +70,11 @@ def homepage():
 		htmlcontent += htmlbody + htmlfooter
 		
 		return (htmlcontent)
-	
-		def logout():
-			response, logoutcontent = http.request(URL + '/logout', 'GET', headers=headers, body=body)
-			return render_template('login.html', error=0)
 	else:
 		error = 1
 		return render_template('login.html', error=error)
 		if(DEBUG):
 			print('Response Status - ', response.status)
-			
-@app.route('/logout')
-def calllogout():
-	return homepage.logout()
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
