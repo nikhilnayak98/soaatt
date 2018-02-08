1bb602141a229419429d352171c7bbdd01603f15
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-09 00:45:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ body = ''
 def my_form():
 	return render_template('login.html', error=0)
 
-@current_app.route('/logout')
-def logout():
-	response, logoutcontent = http.request(URL + '/logout', 'GET', headers=headers, body=body)
-	return render_template('login.html', error=0)
-
 @app.route('/', methods=['POST'])
 def homepage():
 
