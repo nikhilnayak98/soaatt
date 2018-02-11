965cd1b13340d4cf13cddce7d7b8b186cc8bbb54
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-02-11 12:55:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,10 +21,6 @@ def my_form():
 	
 @app.route('/logout')
 def logout():
-	# global var
-	global headers
-	global body
-	
 	response, logoutcontent = http.request(URL + '/logout', 'GET', headers=headers, body=body)
 	return redirect(url_for('my_form'))
 	
